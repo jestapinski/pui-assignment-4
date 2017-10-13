a9453cf0fcac928c35aa56977413c7ed487ad668
Jordan Stapinski <jstapins@andrew.cmu.edu>
2017-10-13 16:27:03 -0400

    Added shopping cart linking and main.js file

---
+++ main.js
@@ -0,0 +1,18 @@
+class Cart {
+	constructor(){
+
+	}
+	// Add
+	// Remove 1
+	// Bulk Remove
+	// Increase Quantity
+	// Load cart
+	// Save cart
+}
+
+class Product {
+	constructor(){
+		// image link
+		// price
+	}
+}
