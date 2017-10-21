8c054985b10bcf1ffbd6012f891dba76e2df9889
Jordan Stapinski <jstapins@andrew.cmu.edu>
2017-10-20 21:15:55 -0400

    Added style

--- cart_render.js
+++ cart_render.js
@@ -1,3 +1,11 @@
+/*
+Jordan Stapinski (jstapins) 
+CMU 05-430 A4
+cart_render.js
+
+The page loading actions for the shopping cart page
+*/
+
 $(document).ready(function() {
     Cart.renderCartItems();
 });
--- items_render.js
+++ items_render.js
@@ -1,3 +1,11 @@
+/*
+Jordan Stapinski (jstapins) 
+CMU 05-430 A4
+items_render.js
+
+The page loading actions for the product detail page
+*/
+
 $(document).ready(function() {
   Product.selectedProductChange(0);
 });
